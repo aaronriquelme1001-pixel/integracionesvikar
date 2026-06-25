9dff2f26c8dc9fab5fcb82a1fcc955b87f6e34be
aaronriquelme1001-pixel <aaronriquelme1001-pixel@users.noreply.github.com>
2026-06-25 19:15:04 -0400

    Cleanup: remove debug/test scripts from repository

--- check_db.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const { Pool } = require('pg');
-const pool = new Pool({ connectionString: process.env.DATALAKE_URL });
-pool.query("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'global_telemetry_traffic' AND column_name = 'dt_tracker'")
-  .then(res => { console.log(res.rows); pool.end(); })
-  .catch(err => { console.error(err); pool.end(); });
